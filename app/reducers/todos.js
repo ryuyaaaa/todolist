d83226425b2d07366b2104a8c7eed55f18ebff19
ryuyasamaaaa <ryuyasamaaaa@gmail.com>
2019-04-28 16:55:47 +0900

    create reducers/todos.js

---
+++ app/reducers/todos.js
@@ -0,0 +1,29 @@
+import {
+    ADD_TODO,
+    TOGGLE_TODO
+} from '../actions/types';
+
+// アクションによる次のステートを返す
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case ADD_TODO:
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                }
+            ]
+        case TOGGLE_TODO:
+            return state.map(todo =>
+                (todo.id == action.id)
+                ? {...todo, completed: !todo.completed}
+                : todo
+                )
+        default:
+            return state
+    }
+}
+
+export default todos;
