e018cc847a7943de31baa6e62ab1d4e205a0b8e1
ryuyasamaaaa <ryuyasamaaaa@gmail.com>
2019-04-28 16:36:39 +0900

    create types.js

---
+++ app/actions/types.js
@@ -0,0 +1,6 @@
+// このファイルではアプリのイベント名あるいはやることを名前付けて管理する
+
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
--- index.js
+++
@@ -1,6 +0,0 @@
-import { AppRegistry } from 'react-native';
-
-// MainContainerを利用するため
-import App from './app/container/Main.js';
-
-AppRegistry.registerComponent('todolist', () => App);
